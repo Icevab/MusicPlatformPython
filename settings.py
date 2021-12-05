cfa02a1fe2d41376a84f537eb3d5111413f0a6ee
dimas <dimasikooooy@gmail.com>
2021-12-05 14:38:13 +0300

    added Song class

--- settings.py
+++ settings.py
@@ -3,11 +3,12 @@ import random
 
 
 class User:
-    def __init__(self, cash, subscription_level, is_subscribed, my_streams, song_list) -> None:
+    def __init__(self, nickname, cash, subscription_level, is_subscribed, my_streams, song_list) -> None:
         # this function is called when a
         # user is created, like x = User()
         # self represents their User object.
         # we can get & set variables on the object
+        self.nickname = nickname
         self.cash = cash
         self.subscription_level = subscription_level
         self.is_subscribed = is_subscribed
@@ -64,19 +65,49 @@ class User:
         print(config.songs[song])
 
     def add_song(self, song_name):
+        """It has two adding command:
+           The first one adds it to config.songs
+           So the recommend_song actually works
+           The second one adds it to song_list of user
+           It is a list with every user's song
+           It gets add as an instance of Song class
+        """
         print(f"You want to add {song_name} right?")
         config.songs.append(song_name)
-        self.song_list.append(song_name)
+        self.song_list.append(
+            Song(song_name=song_name, length=174, author=self.nickname))
         print(f"{song_name} added to songlist!")
         print(config.songs)
+        print(self.song_list)
+
+
+class Song:
+    def __init__(self, song_name, length, author):
+        self.song_name = song_name
+        self.length = length
+        self.author = author
+
+    def __repr__(self):
+        return self.song_name
 
 
 # this calls User.init
-user = User(cash=0, subscription_level=-1, is_subscribed=False,
-            my_streams=9289, song_list=[""])
+song = Song(song_name="Rook B1", length=231, author="s")
+song2 = Song(song_name="Rook B2", length=231, author="cmyui")
+song_list = [song, song2]
+
+user = User(nickname="Boom", cash=0, subscription_level=-1, is_subscribed=False,
+            my_streams=9289, song_list=[song])
+
 
 # then u can use this object
 print(user.is_subscribed)
 
 # and modify it
 user.cash += 69
+
+print(user.money_made())
+
+print(User.add_song.__doc__)
+user.add_song("Le Song")
+print(user.song_list)
